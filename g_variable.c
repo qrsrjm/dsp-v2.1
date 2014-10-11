8b5db918ce360524656871de18abf1ef012ff1b0
qrsrjm <qrsrjm@163.com>
2014-10-11 09:09:48 +0800

    add archive

---
+++ g_variable.c
@@ -0,0 +1,67 @@
+#include "g_variable.h"
+
+
+
+
+
+STR_DSP dspInfo={0};
+STR_DSP *rDspInfo=0;
+
+MACHINE machine;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+#if 0
+
+#ifndef DSPGLOBAL___H___
+#define DSPGLOBAL___H___
+
+enum{
+  Mix_AnDiIn=0,         //analog,digital  input   [crossbar_2]
+  Mix_3D_Start,         //[stereoMixer_1]     
+  Mix4Ch_3D_En_0,       //[Mix4Ch_13]
+  Mix4Ch_3D_En_1,       //[Mix4Ch_14]
+  Mix4Ch_3D_Dly_0,      //[Mix4Ch_11]
+  Mix4Ch_3D_Dly_1,      //[Mix4Ch_12]
+  Mix4Ch_Sct_0,         //[Mix4Ch_2]
+  Mix4Ch_Sct_1,         //[Mix4Ch_10]
+  Mix4Ch_SignalSource,  //[Mix4Ch_3]
+  Mix_Add_SignalSource, //[crossbar_3]
+  Mix_Out               //[crossbar_1]
+};
+
+
+
+#endif //DSPGLOBAL___H___
+
+#endif
